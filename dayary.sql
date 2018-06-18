001a617ea2df86442ae63a8cda77b42b5ed197f0
YoungBum Kim <dudqja1003@gmail.com>
2018-06-18 15:04:52 +0900

    아키텍쳐 설계 1.diary테이블 생성 2.controller, service, dao 작성 3.mybatis-config, mapper작성 4.webjar dependency작성 5.main.html작성

---
+++ dayary.sql
@@ -0,0 +1,16 @@
+create user YBKWeed IDENTIFIED by 11111111;
+
+grant dba to YBKWeed;
+
+create table diary(
+    id number primary key,
+    u_id varchar2(15),
+    title varchar2(50),
+    content clob not null,
+    regdate date not null
+);
+
+create sequence seq_diary_id;
+
+insert into diary values(seq_diary_id.nextval, 'Weed', '첫 번째 일기', '곧 점심시간이다', sysdate);
+insert into diary values(seq_diary_id.nextval, 'Weed', '두 번째 일기', '곧 프로젝트를 시작해야 한다', sysdate);
